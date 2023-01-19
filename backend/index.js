1e3a98f55e8c3bba587ee1fa5ecd678c7c3a4511
Rodrigo Lima <contato@type.dev.br>
2023-01-18 23:33:18 -0300

    Criação da rota de autenticação

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,31 @@
 import express from 'express'
+import jwt from 'jsonwebtoken'
+
+const key = "shgpisaigsgi654646%*%*&¨%"
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  return res.send('Funciona')
+const credentials = {
+  email: 'contato@type.dev.br',
+  senha: '123456'
+}
+
+app.post('/auth', (req, res) => {
+  const { email, senha } = req.body;
+
+  if (email === credentials.email && senha === credentials.senha) {
+    return res.status(200).json({
+      token: jwt.sign({
+        email,
+        role: 'admin'
+      }, key)
+    })
+  }
+
+  return res.status(401).json({
+    error: 'Erro nos dados de autenticação'
+  })
 })
 
 app.listen(3001, () => console.log('Running on port 3001!'))
