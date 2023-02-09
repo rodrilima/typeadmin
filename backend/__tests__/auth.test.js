4fe597cabe39204e1ebff7a2602dde7db0f70f16
Rodrigo Lima <contato@type.dev.br>
2023-02-09 05:38:20 -0300

    test: add 401 and 403 test errors

--- backend/__tests__/auth.test.js
+++ backend/__tests__/auth.test.js
@@ -1,8 +1,8 @@
-import { test, expect } from 'vitest'
+import { it, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../app.js'
 
-test('', async () => {
+it('should be able to authenticate', async () => {
   const credentials = {
     email: 'contato@type.dev.br',
     senha: '123456'
@@ -16,4 +16,32 @@ test('', async () => {
   expect(response.body.token).toEqual(
     expect.any(String)
   )
+})
+
+it('should not be able to authenticate if the password is wrong', async () => {
+  const credentials = {
+    email: 'contato@type.dev.br',
+    senha: '123457'
+  }
+
+  const response = await request(app)
+    .post('/auth')
+    .send(credentials)
+
+  expect(response.status).toEqual(401)
+  expect(response.body.error).toEqual('Authentication data error')
+})
+
+it('should not be able to authenticate if the user does not have authorization', async () => {
+  const credentials = {
+    email: 'rodrigo@type.dev.br',
+    senha: '123456'
+  }
+
+  const response = await request(app)
+    .post('/auth')
+    .send(credentials)
+
+  expect(response.status).toEqual(403)
+  expect(response.body.error).toEqual('User not authorized')
 })
