d43f0f9199955127bd4179013ecba904e5fdb3f7
Rodrigo Lima <contato@type.dev.br>
2023-02-09 05:53:23 -0300

    refactor: export key from app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import cors from 'cors'
 
-const key = "shgpisaigsgi654646%*%*&¨%"
+export const key = "shgpisaigsgi654646%*%*&¨%"
 
 export const app = express()
 app.use(express.json())
