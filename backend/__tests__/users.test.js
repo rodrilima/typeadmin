0300655ed2b3e8c8505a9ac9a78c2ec7cdeb48c2
Rodrigo Lima <contato@type.dev.br>
2023-02-09 05:53:32 -0300

    test: create users test

---
+++ backend/__tests__/users.test.js
@@ -0,0 +1,31 @@
+import { it, expect, beforeAll } from 'vitest'
+import request from 'supertest'
+import { app, key } from '../app.js'
+import jwt from 'jsonwebtoken'
+
+let token;
+beforeAll(() => {
+  token = jwt.sign({}, key)
+})
+
+it('should be able to list users', async () => {
+  const response = await request(app)
+    .get('/users')
+    .set('Authorization', `Bearer ${token}`)
+
+  expect(response.status).toEqual(200)
+  expect(response.body).toEqual(
+    expect.arrayContaining([
+      expect.objectContaining({
+        email: expect.any(String)
+      })
+    ])
+  )
+})
+
+it('should not be able to list users if user is not authenticated', async () => {
+  const response = await request(app)
+    .get('/users')
+
+  expect(response.status).toEqual(401)
+})
